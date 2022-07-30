53c6b3f3bb2476f11c9f761c21036a824ad3bb5f
jcraster <jcraster@student.21-school.ru>
2022-07-30 16:38:06 +0700

    начало парсинга

--- src/graph.cpp
+++ src/graph.cpp
@@ -1,15 +1,50 @@
 #include "graph.h"
-namespace s21{
+namespace s21 {
 
-
-void graph::loadGraphFromFile(std::string filename)
-{
+void graph::loadGraphFromFile(std::string filename) {
+    namesOfNode.clear();
+    adjacencyMatrix.setSize(0);
     std::ifstream file(filename);
-
+    if (!file.is_open()) throw std::invalid_argument("Invalid name file!");
+    getTypeByDirectionAndName(file);
+    std::string lineFile;
+    std::stringstream ss;
+    bool name = true;
+    while (std::getline(file, lineFile)) {
+        ss.clear();
+        ss.str(lineFile);
+    }
 }
 
-void graph::exportGraphToDot(std::string filename)
-{
+void graph::parseLine(bool &name, std::stringstream &ss) {
+    std::string token, prev;
+    ss >> token;
+    while (token.back() != ';') {
+        if (name) {
+            namesOfNode.insert(std::make_pair(token, namesOfNode.size()));
+            adjacencyMatrix(namesOfNode[token],namesOfNode[prev])=1;
+            prev=token;
+        } else if ((token == "->" && typeDirection == TypeGraphByDirection::DIRECTED) ||
+                   (token == "--" && typeDirection == TypeGraphByDirection::UNDIRECRED)) {
+        }
+        name = !name;
+    }
+}
+
+void graph::getTypeByDirectionAndName(std::ifstream &file) {
+    std::string temp;
+    file >> temp;
+    if (temp == "graph")
+        typeDirection = TypeGraphByDirection::UNDIRECRED;
+    else if (temp == "digraph")
+        typeDirection = TypeGraphByDirection::DIRECTED;
+    else
+        throw std::invalid_argument(temp + " - undefined words");
+    file >> name >> temp;
+    if (temp != "{") throw std::invalid_argument("symbol '{' not founded");
+}
+
+void graph::exportGraphToDot(std::string filename) {
     std::ofstream file(filename);
     if (!file.is_open()) {
         throw std::invalid_argument("Invalid name file!");
@@ -22,29 +57,30 @@ void graph::exportGraphToDot(std::string filename)
         file << "graph {" << std::endl;
     }
     Matrix temp = adjacencyMatrix;
-    int sizeMatrix=adjacencyMatrix.size();
+    int sizeMatrix = adjacencyMatrix.size();
     for (int i = 0; i < sizeMatrix; i++) {
         for (int j = 0; j < sizeMatrix; j++) {
-            int one = i + 1, two = j + 1;
-            std::string numberOne = std::to_string(one);
-            std::string numberTwo = std::to_string(two);
-            std::string weight = (typeWeights==TypeGraphByWeights::WEIGHTED)?std::to_string(temp(i,j)):"";
-            if (temp(i, j) != 0) {
-                if (temp(i, j) > 0) {
-                    file << "\t" << numberOne << " " << EdgesInFile << " " << numberTwo << " [" << std::endl;
-                } else {
-                    file << "\t" << numberTwo << " " << EdgesInFile << " " << numberOne << " [" << std::endl;
-                }
-                file << "\t\tlabel = \"" << weight << "\"" << std::endl;
-                file << "\t\tweight = \"" << weight << "\"" << std::endl;
-                file << "\t];" << std::endl;
-                temp(j, i)=temp(j, i)?
-            }
+            printInfoAboutEdge(i + 1, j + 1, file,EdgesInFile,temp);
         }
     }
     file << "}";
     file.close();
 }
 
-
+void graph::printInfoAboutEdge(int firstNode, int secondNode, std::ofstream &file,const std::string& EdgesInFile,const Matrix& temp) {
+    std::string first = std::to_string(firstNode);
+    std::string second = std::to_string(secondNode);
+    std::string weight = (typeWeights == TypeGraphByWeights::WEIGHTED) ? std::to_string(temp(i, j)) : "";
+    if (temp(i, j) != 0) {
+        if (temp(i, j) > 0) {
+            file << "\t" << first << " " << EdgesInFile << " " << second << " [" << std::endl;
+        } else {
+            file << "\t" << second << " " << EdgesInFile << " " << first << " [" << std::endl;
+        }
+        file << "\t\tlabel = \"" << weight << "\"" << std::endl;
+        file << "\t\tweight = \"" << weight << "\"" << std::endl;
+        file << "\t];" << std::endl;
+    }
 }
+
+}  // namespace s21
--- src/graph.h
+++ src/graph.h
@@ -1,7 +1,10 @@
 #include "Matrix.h"
 #include <fstream>
-#include <iostream>
+#include <sstream>
 #include <string>
+#include <map>
+#include <iostream>
+#include <algorithm>
 namespace s21{
 enum class TypeGraphByDirection{
     UNDIRECRED=0,
@@ -15,9 +18,14 @@ enum class TypeGraphByWeights{
 
 class graph{
 private:
+    std::string name;
     Matrix adjacencyMatrix;
     TypeGraphByDirection  typeDirection;
     TypeGraphByWeights typeWeights;
+    std::map<std::string,int> namesOfNode;
+    void printInfoAboutEdge(int firstNode, int secondNode, std::ofstream &file,const std::string& EdgesInFile,const Matrix& temp);
+    void getTypeByDirectionAndName(std::ifstream& file);
+    void parseLine(bool& name,std::stringstream& ss);
 public:
     graph(){}
     void loadGraphFromFile(std::string filename);
